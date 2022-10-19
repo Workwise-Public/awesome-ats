28519aaf9af51d81220549f2e4e6c777cd703621
meduardkrasniqi <meduard.krasniqi@campusjaeger.de>
2022-10-19 22:06:43 +0200

    feat: application structure

---
+++ frontend/src/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Applications } from "./components/applications";
+
+// This component must be a class because its the entry point of the application.
+class App extends React.Component {
+  render(): React.ReactNode {
+    return <>asd</>;
+  }
+}
+
+export default App;
